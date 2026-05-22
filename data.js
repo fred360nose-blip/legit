1ddb8cb3a7c2b078e2aa67d25d12abcad47a58a6
fred360nose-blip <fred360nose@gmail.com>
2026-05-22 12:24:22 +1000

    Create data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+const accounts = [
+  {
+    email: "sample1@test.com",
+    password: "demo123",
+    profileId: "sample1"
+  },
+  {
+    email: "sample2@test.com",
+    password: "demo123",
+    profileId: "sample2"
+  }
+];
+
+const profiles = {
+  sample1: {
+    name: "Sample McSample",
+    dob: "01 Jan 2000",
+    address: "123 Sample Street",
+    licence: "000000000",
+    class: "C"
+  },
+  sample2: {
+    name: "Example Person",
+    dob: "02 Feb 2001",
+    address: "456 Demo Road",
+    licence: "111111111",
+    class: "C"
+  }
+};
